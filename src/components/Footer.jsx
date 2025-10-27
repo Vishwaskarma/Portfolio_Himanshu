c13d6d855e1a4e679b84aca154fa6dc071f752d6
Vishwaskarma <himanshuvishwakarma383@gmail.com>
2025-10-27 15:54:07 +0530

    changes linkedin profile link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
     {
       name: 'LinkedIn',
       icon: Linkedin,
-      url: 'https://linkedin.com/in/himanshu-vishwakarma',
+      url: 'https://www.linkedin.com/in/himanshu-vishwakarma-856773239/',
       username: 'Himanshu Vishwakarma'
     },
     {
